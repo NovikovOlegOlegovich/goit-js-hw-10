5563d7238c689ce1def238ac06cd7c61a6748e21
Novikov Oleh <novikov19962906@gmail.com>
2023-04-24 14:14:29 +0300

    hw-10

---
+++ src/fetchCountries.js
@@ -0,0 +1,25 @@
+export default class FetchCountries {
+  constructor() {
+    this.country = '';
+  }
+
+  fetchCountries() {
+    const fields = '?fields=name,capital,population,flags,languages';
+    const url = `https://restcountries.com/v3.1/name/${this.country}${fields}`;
+
+    return fetch(url).then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+    });
+  }
+
+  get countryName() {
+    return this.country;
+  }
+
+  set countryName(cntr) {
+    this.country = cntr;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 import './css/styles.css';
+import FetchCountries from './fetchCountries.js';
+import debounce from 'lodash.debounce';
+import countrisMurkupLess10 from './templstes/countriesNum2-10.hbs';
+import countrisMurkupFor1 from './templstes/onecountry.hbs';
+import Notiflix from 'notiflix';
+
+refs = {
+  inputEl: document.getElementById('search-box'),
+  countryListEl: document.querySelector('.country-list'),
+};
 
 const DEBOUNCE_DELAY = 300;
+
+const fetchCountriesClass = new FetchCountries();
+
+refs.inputEl.addEventListener('input', debounce(onSerch, DEBOUNCE_DELAY));
+
+function onSerch(event) {
+  fetchCountriesClass.countryName = event.target.value.trim();
+  if (!fetchCountriesClass.countryName) {
+    refs.countryListEl.innerHTML = '';
+    return;
+  }
+  fetchCountriesClass
+    .fetchCountries()
+    .then(country => {
+      if (country.length === 1) {
+        appendCountrisMurkup(country, countrisMurkupFor1);
+      } else if (country.length < 11) {
+        appendCountrisMurkup(country, countrisMurkupLess10);
+      } else if (country.length > 10) {
+        refs.countryListEl.innerHTML = '';
+        Notiflix.Notify.success(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+    })
+    .catch(errors => {
+      refs.countryListEl.innerHTML = '';
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
+}
+
+function appendCountrisMurkup(countris, template) {
+  refs.countryListEl.innerHTML = '';
+  refs.countryListEl.insertAdjacentHTML('beforeend', template(countris));
+}
