acba27ad3263d5ac4ef532d006f718970bf10935
Novikov Oleh <novikov19962906@gmail.com>
2023-04-24 14:50:33 +0300

    hw-10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -8,15 +8,16 @@ export default class FetchCountries {
     const url = `https://restcountries.com/v3.1/name/${this.country}${fields}`;
 
     return fetch(url).then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
     });
   }
 
   get countryName() {
     return this.country;
+    
   }
 
   set countryName(cntr) {
